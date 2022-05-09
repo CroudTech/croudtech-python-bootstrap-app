28e07a059b41493480ea8c10c2da8534c39793a4
Jim Robinson <=>
2022-05-09 09:55:19 +0100

    Add build config

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup
-
-with open("requirements.txt") as f:
+import os
+with open(os.path.join(os.getcwd(), "requirements.txt")) as f:
     required = f.read().splitlines()
 
 setup(name='croudtech-bootstrap',
-      install_requires=required,
-      packages=['croudtech_bootstrap_app'],
-      entry_points={
-          "console_scripts": [
-              "croudtech-bootstrap=croudtech_bootstrap_app.cli:cli"
-          ],
-      },)
+    version=os.getenv("SEMVER", "dev"),
+)
+
