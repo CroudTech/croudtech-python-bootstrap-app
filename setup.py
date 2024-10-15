408ca951216ac0adbc22a73eddede6dea7bcbfd7
Jim Robinson <=>
2024-10-15 15:46:02 +0100

    Use full path for pyinstaller

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup
 
 with open(os.path.join(os.getcwd(), "requirements.txt")) as f:
     required = f.read().splitlines()
-
+with open("./VERSION") as version_file:
+    version = version_file.read()
 setup(
     name="croudtech-bootstrap",
-    version="0.1.43",
+    version=version,
     install_requires=required,
 )
